63f7c121507c17813abe374862ae1af739a2eb94
saiteja218 <saitejareddy218@gmail.com>
2024-08-19 17:33:44 +0530

    Home buttons changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,45 +6,51 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import logo from '../assets/logo.png'
 import { signOut } from 'firebase/auth'
-import {auth} from '../firebase'
-import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase'
+import { useNavigate,useLocation } from 'react-router-dom';
 
 
 function Home() {
-  const navigate=useNavigate();
-  function handleLogout(){
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const getButtonColor = (path) => {
+    return{ color:location.pathname === path ? 'black' : 'inherit'}
+  };
+
+  function handleLogout() {
     signOut(auth)
   }
   return (
     <div>
-      <AppBar  style={{backgroundColor:"#cd232c",margin:"0"}}  position='sticky'>
-       <div style={{width:"90vw",margin:"5px auto"}}>
-        <Toolbar>
-        
+      <AppBar style={{ backgroundColor: "#cd232c", margin: "0" }} position='sticky'>
+        <div style={{ width: "90vw", margin: "5px auto" }}>
+          <Toolbar>
 
-          <Typography  component="div" sx={{ flexGrow:1 ,fontSize:"42px",display:"flex",alignItems:"center"}}>
-          <img src={logo} alt=""  height={40} width={47 } style={{paddingRight:"7px"}}/>
-            <span>CineScope</span>
-            <div style={{display:"inline",marginLeft:"2rem"}}>
-            <Button color="inherit" onClick={()=>{navigate("/home")}}>Home</Button>
-          <Button color="inherit"onClick={()=>{navigate("/myreviews")}}>Reviews</Button>
-          <Button color="inherit" onClick={()=>{navigate("/profile")}}>Profile</Button>
+
+            <Typography component="div" sx={{ flexGrow: 1, fontSize: "42px", display: "flex", alignItems: "center" }}>
+              <img  src={logo} alt="" height={40} width={47} style={{ paddingRight: "7px",cursor:"pointer" }} onClick={() => navigate("/home")}/>
+              <span onClick={() => navigate("/home")} style={{ cursor:"pointer" }}>CineScope</span>
+              <div style={{ display: "inline", marginLeft: "2rem" }}>
+                <Button style={getButtonColor("/home")} onClick={() => navigate("/home")}>Home</Button>
+                <Button style={getButtonColor("/myreviews")} onClick={() => navigate("/myreviews")}>Reviews</Button>
+                <Button style={getButtonColor("/profile")} onClick={() => navigate("/profile")}>Profile</Button>
+              </div>
+            </Typography>
+
+            <div>
+
+              <Button color="inherit" onClick={handleLogout}>Logout</Button>
             </div>
-          </Typography>
-          
-          <div>
-          
-          <Button color="inherit" onClick={handleLogout}>Logout</Button>
-          </div>
-          
-         
-        </Toolbar>
+
+
+          </Toolbar>
         </div>
       </AppBar>
 
-      
+
     </div>
-    
+
   )
 }
 
